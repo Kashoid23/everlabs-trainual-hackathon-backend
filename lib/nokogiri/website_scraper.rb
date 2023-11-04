da8c711bdf786bf62729faeb9c029796c2d248b9
Vitalii Kashoid <kashoyid@gmail.com>
2023-11-04 14:11:33 +0200

    Generate open ai request

---
+++ lib/nokogiri/website_scraper.rb
@@ -0,0 +1,18 @@
+class Nokogiri::WebsiteScraper
+  def self.parse(link:)
+    uri = URI(link)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.open_timeout = 240
+    http.read_timeout = 240
+
+    request = Net::HTTP::Get.new(uri)
+    response = http.request(request)
+
+    if response.is_a?(Net::HTTPSuccess)
+      document = Nokogiri::HTML(response.body)
+
+      Sanitize.fragment(document, elements: %w[h1 h2 h3 h4 h5], remove_contents: %w[script style]).delete!("\n").squeeze(' ')
+    end
+  end
+end
