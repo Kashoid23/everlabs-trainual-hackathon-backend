1834b53b20d23caf16e90d7105051311074538de
Vitalii Kashoid <kashoyid@gmail.com>
2023-11-04 19:32:40 +0200

    Update prompt for V1::AudiosController#create

--- app/controllers/v1/audios_controller.rb
+++ app/controllers/v1/audios_controller.rb
@@ -18,15 +18,18 @@ class V1::AudiosController < ApplicationController
     choices = response.dig("choices")
 
     if choices
-      result = GoogleCloud::TextToSpeech.new(content: choices.dig(0, "message", "content"))
+      choices_json = JSON.parse(choices.dig(0, "message", "content"))
+      result = GoogleCloud::TextToSpeech.new(content: choices_json.dig("content"))
 
       return render json: result.error unless result.success?
+
+      filename = choices_json.dig("title").parameterize(separator: '_')
 
       GoogleCloud::Storage.upload_file(filename: "#{filename}.mp3", content: result.audio_content)
 
       audio = Audio.new(
         folder_id: params[:folder_id],
-        title: filename.humanize,
+        title: choices_json.dig("title"),
         link: link,
         src: "https://storage.googleapis.com/hackaton-trainual/#{filename}.mp3"
       )
@@ -53,7 +56,11 @@ class V1::AudiosController < ApplicationController
         "role": "system",
         content: [
           'You are a helpful AI writing assistant',
-          'Analyze the provided website content and provide a text summary - 200 words maximum'
+          'Follow these instructions:',
+          'Step 1 - The user will provide you with sanitized content. Respond with structured data as shown in the example',
+          'Example: title: "...", content: "..."',
+          'Step 2 - Analyze the provided website content and provide short title - 4 words maximum and a content summary - 200 words maximum',
+          'Step 3 - Your response should be a JSON with no newline characters or anything else',
         ].join('. ')
       },
       {
@@ -69,12 +76,6 @@ class V1::AudiosController < ApplicationController
 
   def folder_id_param
     params[:folder_id] || Folder.first.id
-  end
-
-  def filename
-    url_object = URI.parse(link)
-    url_path = url_object.path
-    "#{url_path.split("/").last}"
   end
 
   def link
