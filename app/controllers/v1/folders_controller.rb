c77de1483dbedebb60bada934607406e8c8d8ab6
Vitalii Kashoid <kashoyid@gmail.com>
2023-11-05 03:41:47 +0200

    Get all folders when params[:all] present

--- app/controllers/v1/folders_controller.rb
+++ app/controllers/v1/folders_controller.rb
@@ -1,5 +1,7 @@
 class V1::FoldersController < ApplicationController
   def index
+    return render json: Folder.all if params[:all].present?
+
     render json: Folder.joins(:audios).group("folders.id").select("folders.*, count(audios.id) as audios_count")
   end
 
